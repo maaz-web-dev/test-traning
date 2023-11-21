2090aa0434bfd7c1639dc898fc2c59fcc3711249
maaz-web-dev <maazkhaansofteng@gmail.com>
2023-11-21 15:51:10 -0500

    added paganation

--- src/components/ExpiringTrainings.js
+++ src/components/ExpiringTrainings.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ExpiringTrainings({ trainingData, specifiedDate }) {
+  const itemsPerPage = 20; // Number of items to display per page
+  const [currentPage, setCurrentPage] = useState(1);
+
   const getExpiringTrainings = () => {
     const results = [];
 
@@ -33,6 +36,28 @@ function ExpiringTrainings({ trainingData, specifiedDate }) {
 
   const expiringTrainings = getExpiringTrainings();
 
+  // Calculate the total number of pages
+  const totalPages = Math.ceil(expiringTrainings.length / itemsPerPage);
+
+  // Calculate the index range for the current page
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  // Get the items to display for the current page
+  const itemsToDisplay = expiringTrainings.slice(startIndex, endIndex);
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
   return (
     <div>
       <h2>Expiring Trainings</h2>
@@ -45,7 +70,7 @@ function ExpiringTrainings({ trainingData, specifiedDate }) {
           </tr>
         </thead>
         <tbody>
-          {expiringTrainings.map((result, index) => (
+          {itemsToDisplay.map((result, index) => (
             <tr key={index}>
               <td>{result.personName}</td>
               <td>{result.trainingName}</td>
@@ -54,7 +79,30 @@ function ExpiringTrainings({ trainingData, specifiedDate }) {
           ))}
         </tbody>
       </table>
+
+      <div className="pagination">
+      <button
+        className="pagination-button"
+        onClick={handlePreviousPage}
+        disabled={currentPage === 1}
+      >
+        &lt; Previous
+      </button>
+      <span className="page-count">
+        {`Showing ${startIndex + 1}-${Math.min(
+          endIndex,
+          expiringTrainings.length
+        )} of ${expiringTrainings.length}`}
+      </span>
+      <button
+        className="pagination-button"
+        onClick={handleNextPage}
+        disabled={currentPage === totalPages}
+      >
+        Next &gt;
+      </button>
     </div>
+  </div>
   );
 }
 
